437e5bb9d1a2526f3c4420a68c8c9f8898d7ae34
DiegoJohann <diego9.roberto7@gmail.com>
2024-06-06 14:24:36 -0300

    Métodos Equals e HashCode da classe PedidoRevendaID adicionados.

--- src/main/java/br/com/wirth/apiprecocorreios/domain/entities/PedidoRevendaID.java
+++ src/main/java/br/com/wirth/apiprecocorreios/domain/entities/PedidoRevendaID.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Embeddable
@@ -12,4 +13,16 @@ public class PedidoRevendaID implements Serializable {
     private String pedido;
     private String cmaterialcombo;
     private String item;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PedidoRevendaID that)) return false;
+        return Objects.equals(pedido, that.pedido) && Objects.equals(item, that.item);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pedido, item);
+    }
 }
